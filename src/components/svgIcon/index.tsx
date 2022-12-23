4724ecacbd373a8a0e7d285387fe3b32521e1350
zhengp <zhengp>
2022-12-23 17:55:44 +0800

    [fix] 1、修改 svg 加不上样式

--- src/components/svgIcon/index.tsx
+++ src/components/svgIcon/index.tsx
@@ -1,5 +1,13 @@
 import { defineComponent, computed } from "vue";
 import style from "./index.module.scss";
+// TODO css module svg 加不上样式
+const svgIcon = {
+  width: "2em",
+  height: "2em",
+  verticalAlign: "-0.15em",
+  overflow: "hidden",
+  fill: "currentColor",
+};
 export default defineComponent({
   props: {
     name: String,
@@ -11,7 +19,7 @@ export default defineComponent({
     // TODO css module svg 加不上样式
     return () => (
       <>
-        <svg aria-hidden="true" className={style.svgIcon}>
+        <svg aria-hidden="true" className={style.svgIcon} style={svgIcon}>
           <use xlink:href={symbolId.value} fill={color} />
         </svg>
       </>
