c2d2e8faa5381b59d3ff3ddad859279d3591fe4b
zhengp <linzhe141@qq.com>
2023-01-13 13:06:35 +0800

    [feat] 优化两数相加(linked-list)

--- src/leetcode/linkedList/addTwoNumbers.ts
+++ src/leetcode/linkedList/addTwoNumbers.ts
@@ -2,113 +2,70 @@ import { ListNode } from "@/types/ListNode";
 function init() {
   // const l1 = new ListNode(2, new ListNode(4, new ListNode(3, new ListNode(1))));
   // const l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
-  // const l1 = new ListNode(
-  //   9,
-  //   new ListNode(
-  //     9,
-  //     new ListNode(
-  //       9,
-  //       new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9))))
-  //     )
-  //   )
-  // );
-  // const l2 = new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9))));
-  const l1 = new ListNode(5);
-  const l2 = new ListNode(5);
+  const l1 = new ListNode(
+    9,
+    new ListNode(
+      9,
+      new ListNode(
+        9,
+        new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9))))
+      )
+    )
+  );
+  const l2 = new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9))));
+  // const l1 = new ListNode(5);
+  // const l2 = new ListNode(5);
   return [l1, l2];
 }
 const [l1, l2] = init();
-const addTwoNumbers = (
+function addTwoNumbers(
   l1: ListNode | null,
   l2: ListNode | null
-): ListNode | null => {
+): ListNode | null {
   let result = new ListNode();
-  let head = null;
   let current = null;
   let l1Index: ListNode | null = l1;
   let l2Index: ListNode | null = l2;
-  let tenFalg = false;
+  let tenFalg = 0;
   while (l1Index || l2Index) {
-    let resultNode: ListNode = new ListNode();
-    if (l1Index && l2Index) {
-      if (!tenFalg) {
-        if (l1Index.val + l2Index.val < 10) {
-          resultNode.val = l1Index.val + l2Index.val;
-          tenFalg = false;
-        } else {
-          resultNode.val = (l1Index.val + l2Index.val) % 10;
-          tenFalg = true;
-        }
+    let node: ListNode = new ListNode();
+    const temp1 = l1Index ?? new ListNode();
+    const temp2 = l2Index ?? new ListNode();
+    const timeVal = temp1.val + temp2.val;
+    if (!tenFalg) {
+      if (timeVal < 10) {
+        node.val = timeVal;
+        tenFalg = 0;
       } else {
-        if (l1Index.val + l2Index.val + 1 < 10) {
-          resultNode.val = l1Index.val + l2Index.val + 1;
-          tenFalg = false;
-        } else {
-          resultNode.val = (l1Index.val + l2Index.val + 1) % 10;
-          tenFalg = true;
-        }
+        node.val = timeVal % 10;
+        tenFalg = 1;
       }
-    }
-    if (l1Index && !l2Index) {
-      if (!tenFalg) {
-        if (l1Index.val < 10) {
-          resultNode.val = l1Index.val;
-          tenFalg = false;
-        } else {
-          resultNode.val = l1Index.val % 10;
-          tenFalg = true;
-        }
+    } else {
+      if (timeVal + 1 < 10) {
+        node.val = timeVal + 1;
+        tenFalg = 0;
       } else {
-        if (l1Index.val + 1 < 10) {
-          resultNode.val = l1Index.val + 1;
-          tenFalg = false;
-        } else {
-          resultNode.val = (l1Index.val + 1) % 10;
-          tenFalg = true;
-        }
-      }
-    }
-    if (!l1Index && l2Index) {
-      if (!tenFalg) {
-        if (l2Index.val < 10) {
-          resultNode.val = l2Index.val;
-          tenFalg = false;
-        } else {
-          resultNode.val = l2Index.val % 10;
-          tenFalg = true;
-        }
-      } else {
-        if (l2Index.val + 1 < 10) {
-          resultNode.val = l2Index.val + 1;
-          tenFalg = false;
-        } else {
-          resultNode.val = (l2Index.val + 1) % 10;
-          tenFalg = true;
-        }
+        node.val = (timeVal + 1) % 10;
+        tenFalg = 1;
       }
     }
     if (l1Index?.next) l1Index = l1Index.next;
     else l1Index = null;
     if (l2Index?.next) l2Index = l2Index.next;
     else l2Index = null;
-    if (head === null) {
-      result = resultNode;
-      head = result;
-      current = head;
+    if (current === null) {
+      result = node;
+      current = result;
     } else {
-      current = head;
-      while (current.next) {
+      if (current) {
+        current.next = node;
         current = current.next;
       }
-      current.next = resultNode;
     }
   }
   if (tenFalg && current) {
-    while (current.next) {
-      current = current.next;
-    }
     current.next = new ListNode(1);
   }
   return result;
-};
+}
 console.log(addTwoNumbers(l1, l2));
